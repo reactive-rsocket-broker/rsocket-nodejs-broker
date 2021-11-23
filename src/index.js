185cf123b1099fc2739c8619a9aea191d606de47
linux_china <libing.chen@gmail.com>
2021-11-23 15:46:03 -0800

    broker server

---
+++ src/index.js
@@ -0,0 +1,48 @@
+const {RSocketServer, CompositeMetadata} = require('rsocket-core');
+const RSocketWebSocketServer = require('rsocket-websocket-server');
+const {Single} = require('rsocket-flowable');
+const {v4: uuidv4} = require('uuid');
+
+// active rsocket connections
+const CONNECTIONS = new Map();
+
+const requestHandler = (requestingRSocket, setupPayload) => {
+    // todo parse setup payload and inject request rsocket to global connections
+    //const compositeMetadata = new CompositeMetadata(setupPayload.metadata );
+    let connectionId = uuidv4();
+    // rsocket connection status subscribe
+    requestingRSocket.connectionStatus().subscribe({
+        onNext: status => {
+            if (status.kind === 'CLOSED' || status.kind === 'ERROR') {
+                console.log("connection closed");
+                CONNECTIONS.delete(connectionId);
+            }
+        },
+        onSubscribe: subscription => subscription.request(Number.MAX_SAFE_INTEGER)
+    });
+    // metadata push
+    requestingRSocket.metadataPush({metadata: JSON.stringify({})}).subscribe();
+    // add to connections
+    requestingRSocket.uuid = connectionId;
+    CONNECTIONS.set(connectionId, requestingRSocket);
+    // responder
+    return {
+        requestResponse(payload) {
+            // todo forward request to destination
+            //const compositeMetadata = new CompositeMetadata(payload.metadata );
+            return Single.of({
+                data: "Hello " + payload.data
+            });
+        },
+        metadataPush(payload) {
+            return Single.of({});
+        },
+    };
+};
+
+const WebSocketServerTransport = RSocketWebSocketServer.default;
+const transport = new WebSocketServerTransport({host: "127.0.0.1", port: 42252});
+const rsocketServer = new RSocketServer({transport: transport, getRequestHandler: requestHandler});
+rsocketServer.start();
+
+
