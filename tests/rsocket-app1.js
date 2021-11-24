1e82eb7ca2549e6e912c9b66f0f40b08b67c1ec4
linux_china <libing.chen@gmail.com>
2021-11-23 16:06:07 -0800

    rsocket-app1.js

---
+++ tests/rsocket-app1.js
@@ -0,0 +1,62 @@
+const RSocketWebSocketClient = require('rsocket-websocket-client').default;
+const {RSocketClient} = require("rsocket-core");
+const WebSocket = require('ws');
+const {Single} = require("rsocket-flowable");
+
+const rsocketClient = new RSocketClient({
+    setup: {
+        keepAlive: 1000000,
+        lifetime: 100000,
+        metadataMimeType: 'message/x.rsocket.composite-metadata.v0',
+        dataMimeType: 'application/json',
+        payload: {
+            data: JSON.stringify({ip: '192.168.1.1', port: 8181, name: 'rsocket-app1'}),
+        }
+    },
+    transport: new RSocketWebSocketClient(
+        {
+            debug: true,
+            url: 'ws://localhost:42252',
+            wsCreator: url => new WebSocket(url)
+        }
+    ),
+    responder: {
+        requestResponse(payload) {
+            return new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    resolve({
+                        data: {
+                            message: 'Hello, ' + payload.data.name + '!'
+                        }
+                    });
+                }, 1000);
+            });
+        },
+        fireAndForget(payload) {
+            console.log('fireAndForget', payload.data);
+        },
+        metadataPush(payload) {
+            if (payload.metadata) {
+                console.log('metadataPush', payload.metadata);
+                rsocketClient.uuid = JSON.parse(payload.metadata).uuid;
+            }
+            return Single.of({});
+        },
+    }
+});
+
+const monoRSocket = rsocketClient.connect();
+
+monoRSocket.then(rsocket => {
+    rsocket.requestResponse({
+        data: "leijuan",
+        metadata: ''
+    }).subscribe({
+        onComplete: (payload) => console.log(payload),
+        onError: error => {
+            console.error(error);
+        },
+    });
+});
+
+
