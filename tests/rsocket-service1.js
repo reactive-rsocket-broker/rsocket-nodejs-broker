0ed4c6c36efff3ed796b514b373b9728cdda7eee
linux_china <libing.chen@gmail.com>
2021-11-25 14:16:41 -0800

    Implement as service provider

---
+++ tests/rsocket-service1.js
@@ -0,0 +1,51 @@
+const RSocketWebSocketClient = require('rsocket-websocket-client').default;
+const {RSocketClient, APPLICATION_JSON} = require("rsocket-core");
+const WebSocket = require('ws');
+const {Single} = require("rsocket-flowable");
+
+const BROKER_URL = "ws://localhost:42252";
+const appMetadata = {ip: '192.168.1.2', name: 'rsocket-app2', services: ['com.example.UserService']};
+
+const rsocketClient = new RSocketClient({
+    setup: {
+        keepAlive: 1000000,
+        lifetime: 100000,
+        metadataMimeType: APPLICATION_JSON._string,
+        dataMimeType: APPLICATION_JSON._string,
+        payload: {
+            data: JSON.stringify(appMetadata)
+        }
+    },
+    transport: new RSocketWebSocketClient(
+        {
+            debug: true,
+            url: BROKER_URL,
+            wsCreator: (url) => new WebSocket(url)
+        }
+    ),
+    responder: {
+        requestResponse(payload) {
+            return Single.of({
+                data: JSON.stringify({id: 1, name: 'John Doe'}),
+            })
+        },
+        fireAndForget(payload) {
+            console.log('fireAndForget', payload.data);
+        },
+        metadataPush(payload) {
+            if (payload.metadata) {
+                console.log('metadataPush', payload.metadata);
+                appMetadata.uuid = JSON.parse(payload.metadata).uuid;
+            }
+            return Single.of({});
+        },
+    }
+});
+
+const monoRSocket = rsocketClient.connect();
+
+monoRSocket.then(_rsocket => {
+    console.log(`Node.js RSocket Service started and connected with ${BROKER_URL}`);
+});
+
+
